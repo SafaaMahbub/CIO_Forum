c34039063a5932ded06b94ce0d4042f219e98ba2
alvinrhee <100805250+alvinrhee@users.noreply.github.com>
2026-02-16 15:23:12 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("Django is running on Heroku 🚀")),
     path('admin/', admin.site.urls),
 ]
