d0f8309fa7da7b1e74ccf76c9f072cff2b0afeec
edwar <edwardma030815gmail.com>
2026-03-27 23:55:37 -0400

    make alert messages to popup windows

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(() =>
             {
                 alert.remove();
-                const container = document.querySelector('.messages');
-                if (container && container.children.length === 0) {
-                    container.remove();
-                    container.visible = False;
+                // check if ANY alerts remain
+                if (document.querySelectorAll('.messages .alert').length === 0) {
+                    const container = document.querySelector('.messages');
+                    if (container) container.remove();
                 }
             }, 1000);
         }, 2500);
